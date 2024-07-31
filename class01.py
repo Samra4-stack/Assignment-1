685fee8bf330984314e2309744c1486eab669eb6
Samra4-stack <samratariq4544@gmail.com>
2024-07-31 12:00:09 +0500

    Add files via upload

---
+++ class01.py
@@ -0,0 +1,12 @@
+# Assignment 1
+print('''Myself Essay
+   Hello! My name is Samra Tariq.
+   I am from Faisalabad.
+    I am currently 19 years old
+    I enjoy watching movies.
+    I am studying BS-CS at UAF.
+    In the future, I hope to develop something new in the field of AI.
+    I am very passionate about this Field.
+    I am new in this field.
+    Thank you for reading my essay!''')
+
